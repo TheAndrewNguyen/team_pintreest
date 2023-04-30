1ecdf16f877f734f6c8894c002f60cf366829db3
Jessica-YChen <124203629+Jessica-YChen@users.noreply.github.com>
2023-04-30 13:49:45 -0700

    Update show.blade.php

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,23 +1,22 @@
 <x-app-layout>
 
     <div class="mx-auto p-4 sm:p-6 lg:p-8">
-        <div>
-            <p>{{$user->name}}</p>
+        <div style="text-align: center; width: 100%;">
+            <p style="font-size: 64px; font-weight: 700;">{{$user->name}}</p>
             <p>{{$user->email}}</p>
-            <p>{{$user->email_verified_at}}</p>
+            <!-- <p>{{$user->email_verified_at}}</p> -->
         </div>
 
-        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y flex" style="max-width:100%; flex-wrap:wrap;">
+        <p style="font-size: 36px; text-align: center; width: 100%;"> My posts </p>
+
+        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y flex" style="max-width:100%; flex-wrap:wrap; background-color: #cee8cc; ">
             @include('profile.partials.my-posts')
         </div>
+        
+        <p style="font-size: 36px; text-align: center; width: 100%; margin-top: 32px;"> Saved posts </p>
 
-        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y flex" style="max-width:100%; flex-wrap:wrap;">
+        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y flex" style="max-width:100%; flex-wrap:wrap; background-color: #cee8cc; ">
             @include('profile.partials.saved-posts')
         </div>
-
-        saved: 
-        @foreach($user->saved as $post) 
-            {{$post}}
-        @endforeach
     </div>
 </x-app-layout>
