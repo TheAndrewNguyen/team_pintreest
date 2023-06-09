da02f74c80485634ee1abdb814df040a3ed49b9c
SamGanYX <samganyx@gmail.com>
2023-06-08 23:39:08 -0700

    Update show.blade.php

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -12,11 +12,12 @@
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y flex" style="max-width:100%; flex-wrap:wrap; background-color: #cee8cc; ">
             @include('profile.partials.my-posts')
         </div>
-        
+        @if($user->saved)
         <p style="font-size: 36px; text-align: center; width: 100%; margin-top: 32px;"> Saved posts </p>
 
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y flex" style="max-width:100%; flex-wrap:wrap; background-color: #cee8cc; ">
             @include('profile.partials.saved-posts')
         </div>
+        @endif
     </div>
 </x-app-layout>
